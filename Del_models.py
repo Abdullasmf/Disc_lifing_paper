426c040e60c970d08d6ce7dc2823a173e4bec170
AbdullaSMF <AbdullaSMF@gmail.com>
2026-07-22 10:47:00 +0000

    New models

--- Del_models.py
+++ Del_models.py
@@ -15,8 +15,6 @@ for folder in folders1:
     #folders in folder
     subfolders = [f for f in folder.iterdir() if f.is_dir()]
     for subfolder in subfolders:
-        if "ArGEnT" in str(subfolder):
-            continue
         ptPath = Path(subfolder, "Trained_models")
         #glob for .pt files in ptPath
         pt_files = glob.glob(str(ptPath) + "/*.pt")
@@ -26,8 +24,6 @@ for folder in folders2:
     #folders in folder
     subfolders = [f for f in folder.iterdir() if f.is_dir()]
     for subfolder in subfolders:
-        if "ArGEnT" in str(subfolder):
-            continue
         ptPath = Path(subfolder, "Trained_models")
         #glob for .pt files in ptPath
         pt_files = glob.glob(str(ptPath) + "/*.pt")
